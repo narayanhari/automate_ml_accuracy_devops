c4fb983843d3b6e942ea6052e5fe02a081fc8bc0
Narayan Hari <34136060+narayanhari@users.noreply.github.com>
2020-05-14 22:11:02 +0530

    overwrite file added

---
+++ overwrite.py
@@ -0,0 +1,29 @@
+import os
+file1 = open("main_accuracy.txt","r")
+file2 = open("test_accuracy.txt","r")
+if file1.readline():
+    a = True
+else:
+    a = False
+file1.close()
+
+#print (float(file2.readline()))
+a2 = float(file2.readline())
+if not a:
+    file1 = open("main_accuracy.txt","w")
+    
+    file1.write(str(a2))
+    file1.close()
+    print(2)
+else:
+    file1 = open("main_accuracy.txt","r")
+    a1 = float(file1.readline())
+    file1.close()
+    
+    if a1<a2:
+        file1 = open("main_accuracy.txt","w")
+        file1.write(str(a2))
+        file1.close()
+
+
+file2.close()
